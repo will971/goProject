22ef02cc677e23dff62166bae7fc3cd9c93a21a7
will <modeste.william.s@gmail.com>
2019-03-17 15:29:30 +0100

    maj html

--- main.go
+++ main.go
@@ -21,17 +21,14 @@ func setHandler() {
 	fs := http.FileServer(http.Dir("./static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", fs))
 	r.HandleFunc("/", indexHandler).Methods("GET")
-	r.HandleFunc("/about", aboutHandler).Methods("GET")
-	r.HandleFunc("/blog", blogHandler).Methods("GET")
+	r.HandleFunc("/qui-utilise-go", goUsersHandler).Methods("GET")
 	r.HandleFunc("/contact", contactHandler).Methods("GET")
 	r.HandleFunc("/post", postHandler).Methods("GET")
-	r.HandleFunc("/project", projectHandler).Methods("GET")
 	r.HandleFunc("/creators/Robert-Griesmer", robertGriesmerHandler).Methods("GET")
 	r.HandleFunc("/creators/Rob-Pike", robPikeHandler).Methods("GET")
 	r.HandleFunc("/creators/Ken-Thompson", kenThompsonHandler).Methods("GET")
 	r.HandleFunc("/go/code", goCodeHandler).Methods("GET")
 	r.HandleFunc("/go/routines", goRoutinesHandler).Methods("GET")
-
 	http.Handle("/", r)
 	http.Handle("/godBy", r)
 	http.ListenAndServe(":8080", nil)
@@ -40,26 +37,20 @@ func setHandler() {
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", nil)
 }
-func blogHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "blog.html", nil)
-}
 func goRoutinesHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "goRoutines.html", nil)
 }
 func goCodeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "goCode.html", nil)
 }
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "about.html", nil)
+func goUsersHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "goUsers.html", nil)
 }
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "contact.html", nil)
 }
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "single-post.html", nil)
-}
-func projectHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "single-project.html", nil)
 }
 func robPikeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "robPike.html", nil)
